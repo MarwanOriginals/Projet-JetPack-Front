249309e30dd4a0b9b6ffcf5aaaa85479e434058e
Yoan <yoanboit@hotmail.fr>
2019-11-11 18:56:51 +0100

    Modification du CSS + Test de la fonction save()

--- src/Service/Api/jetpackApi.test.js
+++ src/Service/Api/jetpackApi.test.js
@@ -39,7 +39,54 @@ describe('JetpackApi get Jetpacks', function () {
     });
 });
 
-describe('JetpackApi search Jetpacks', function () {
+describe('JetpackApi create Jetpack', function () {
+
+  test('Test save no data', () => {
+      let httpClientMock = {
+          fetch: jest.fn()
+      };
+
+      let jetpack = new Jetpack();
+
+      httpClientMock.fetch.mockResolvedValue();
+
+      let jetpackApi = new JetpackApi(httpClientMock);
+      jetpackApi.save(jetpack).then(resp => {
+          expect(resp).toBeInstanceOf(Jetpack);
+          expect(resp.id).toBeNull();
+      });
+  });
+
+  test('Test save a Jetpack', () => {
+      let httpClientMock = {
+          fetch: jest.fn()
+      };
+
+      httpClientMock.fetch.mockResolvedValue([
+            {
+              "id": "e8019ec0-bfdc-4140-9dbb-4927e5ef5c8d",
+              "name": "Le jetpack",
+              "image": "Image 3"
+            }
+        ]);
+
+      let jetpack = new Jetpack();
+      jetpack.id = "e8019ec0-bfdc-4140-9dbb-4927e5ef5c8d";
+      jetpack.name = "Le jetpack";
+      jetpack.image = "Image 3";
+
+      httpClientMock.fetch.mockResolvedValue();
+
+      let jetpackApi = new JetpackApi(httpClientMock);
+      jetpackApi.save(jetpack).then(resp => {
+          expect(resp).toBeInstanceOf(Jetpack);
+          expect(resp).toBeEqual(jetpack);
+      });
+  });
+
+});
+
+describe('JetpackApi search for Jetpacks', function () {
 
   test('Test getBookings empty', () => {
       let httpClientMock = {
